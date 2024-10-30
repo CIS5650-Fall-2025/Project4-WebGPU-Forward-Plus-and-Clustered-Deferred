8d9cb8a0443cf76229aa46b1cddf5b0f1d2c4704
Nadine Adnane <nadnane@users.noreply.github.com>
2024-10-30 02:11:43 -0400

    WIP forward plus

--- src/renderers/forward_plus.ts
+++ src/renderers/forward_plus.ts
@@ -5,16 +5,20 @@ import { Stage } from '../stage/stage';
 export class ForwardPlusRenderer extends renderer.Renderer {
     // TODO-2: add layouts, pipelines, textures, etc. needed for Forward+ here
     // you may need extra uniforms such as the camera view matrix and the canvas resolution
+    sceneUniformsBindGroupLayout: GPUBindGroupLayout;
+    sceneUniformsBindGroup: GPUBindGroup;
+
+    depthTexture: GPUTexture;
+    depthTextureView: GPUTextureView;
+
+    forwardPlusPipeline: GPURenderPipeline;
 
     constructor(stage: Stage) {
         super(stage);
-
         // TODO-2: initialize layouts, pipelines, textures, etc. needed for Forward+ here
     }
 
     override draw() {
         // TODO-2: run the Forward+ rendering pass:
-        // - run the clustering compute shader
-        // - run the main rendering pass, using the computed clusters for efficient lighting
     }
 }
--- src/shaders/shaders.ts
+++ src/shaders/shaders.ts
@@ -27,10 +27,9 @@ export const constants = {
     bindGroup_scene: 0,
     bindGroup_model: 1,
     bindGroup_material: 2,
-
     moveLightsWorkgroupSize: 128,
-
-    lightRadius: 2
+    lightRadius: 2,
+    maxLightPerCluster: 256,
 };
 
 // =================================
