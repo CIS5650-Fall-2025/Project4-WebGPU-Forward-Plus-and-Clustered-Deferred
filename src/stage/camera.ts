cebc8905751852662babd439dd48b81aea72e13a
Nadine Adnane <nadnane@users.noreply.github.com>
2024-12-29 15:30:15 -0500

    Camera refactor

--- src/stage/camera.ts
+++ src/stage/camera.ts
@@ -3,38 +3,39 @@ import { toRadians } from "../math_util";
 import { device, canvas, fovYDegrees, aspectRatio } from "../renderer";
 
 class CameraUniforms {
-    // Define the camera buffer, 412 bytes + 4 bytes of padding
-    readonly camBuffer = new ArrayBuffer(416);
-
-    // Define view matrices
-    // Each is a 4x4 matrix = 16 Float32Array elements
-    // 6 matrices x 16 elements each = 96 Float32Array elements
-    // Each value in the Float32Array takes 4 bytes
-    viewProjMatVals = new Float32Array(this.camBuffer, 0, 16);   
-    invViewProjMatVals = new Float32Array(this.camBuffer, 64, 16);   // 0 + (16 elements * 4 bytes) = 64
-
-    // Define view matrix and inverse view matrix
-    viewMatVals = new Float32Array(this.camBuffer, 128, 16);         // 64 + (16 * 4) = 128
-    invViewMatVals = new Float32Array(this.camBuffer, 192, 16);      // 128 + (16 * 4) = 192
+    // Define the camera buffer, 208 bytes
+    readonly camBuffer = new ArrayBuffer(
+        16 * 4 +   // view-projection matrix, 16 elements 4 bytes each
+        4 * 4  +   // camera vector, 4 elements 4 bytes each
+        16 * 4 +   // inverse projection matrix, 16 elements 4 bytes each
+        16 * 4     // inverse view matrix, 16 elements 4 bytes each
+    );
     
-    projMatVals = new Float32Array(this.camBuffer, 256, 16);         // 192 + (16 * 4) = 256
-    invProjMatVals = new Float32Array(this.camBuffer, 320, 16);      // 256 + (16 * 4) = 320
-    
-    // Define additional view values
-    // 96 elements * 4 bytes each = 384
-    eyePosVals = new Float32Array(this.camBuffer, 384, 3);           // 320 + (16 * 4) = 384
-    nearPlaneVal = new Float32Array(this.camBuffer, 396, 1);         // 384 + (3 * 4) = 396
-    farPlaneVal = new Float32Array(this.camBuffer, 400, 1);          // 396 + 4 = 400
+    // Store all values directly in Float32Array
+    private readonly floatArray = new Float32Array(this.camBuffer);
 
-    // Define screen dims
-    screenWidthVal = new Float32Array(this.camBuffer, 404, 1);       // 400 + 4 = 404
-    screenHeightVal = new Float32Array(this.camBuffer, 408, 1);      // 404 + 4 = 408, ends at 408 + 4 = 412 bytes
+    //==========================================================================
+    // Define setters for each property
+    set viewProjMat(matrix: Float32Array) {
+        this.floatArray.set(matrix, 0);
+    }
+    set camera(value: Float32Array) {
+        this.floatArray.set(value, 16);
+    }
+    set inverse_projection(matrix: Float32Array) {
+        this.floatArray.set(matrix, 20);
+    }
+    set inverse_view(matrix: Float32Array) {
+        this.floatArray.set(matrix, 36);
+    }
+    //==========================================================================
 }
 
 export class Camera {
     uniforms: CameraUniforms = new CameraUniforms();
     uniformsBuffer: GPUBuffer;
 
+    // Define camera properties
     projMat: Mat4 = mat4.create();
     cameraPos: Vec3 = vec3.create(-7, 2, 0);
     cameraFront: Vec3 = vec3.create(0, 0, -1);
@@ -45,30 +46,20 @@ export class Camera {
     moveSpeed: number = 0.004;
     sensitivity: number = 0.15;
 
-    // Define screen dims
-    height: number = 2056;
-    width: number = 1600;
-
     static readonly nearPlane = 0.1;
-    static readonly farPlane = 33;
+    static readonly farPlane = 1000;
 
     keys: { [key: string]: boolean } = {};
 
     constructor () {
-        // DONE-1.1: set `this.uniformsBuffer` to a new buffer of size `this.uniforms.buffer.byteLength`
-        // ensure the usage is set to `GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST` since we will be copying to this buffer
-        // check `lights.ts` for examples of using `device.createBuffer()`
-        //
-        // note that you can add more variables (e.g. inverse proj matrix) to this buffer in later parts of the assignment
+        
+        // Define camera uniform buffer
         this.uniformsBuffer = device.createBuffer({
+            label: "camera",
             size: this.uniforms.camBuffer.byteLength,
             usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
         });
-
-        // DONE-2: initialize extra buffers needed for light clustering here
-        this.uniforms.nearPlaneVal[0] = Camera.nearPlane;
-        this.uniforms.farPlaneVal[0] = Camera.farPlane;
-
+        
         this.projMat = mat4.perspective(toRadians(fovYDegrees), aspectRatio, Camera.nearPlane, Camera.farPlane);
 
         this.rotateCamera(0, 0); // set initial camera vectors
@@ -154,31 +145,26 @@ export class Camera {
 
     onFrame(deltaTime: number) {
         this.processInput(deltaTime);
-    
-        // Calculate matrix values and intermediates
+
         const lookPos = vec3.add(this.cameraPos, vec3.scale(this.cameraFront, 1));
         const viewMat = mat4.lookAt(this.cameraPos, lookPos, [0, 1, 0]);
-        const invViewMat = mat4.invert(viewMat);
-        const projMat = this.projMat;
-        const invProjMat = mat4.invert(projMat);
-        const viewProjMat = mat4.mul(projMat, viewMat);
-        const invViewProjMat = mat4.invert(viewProjMat);
-    
-        // DONE-2: write to extra buffers needed for light clustering here
-        this.uniforms.viewProjMatVals.set(viewProjMat);
-        this.uniforms.invViewProjMatVals.set(invViewProjMat);
-        this.uniforms.viewMatVals.set(viewMat);
-        this.uniforms.invViewMatVals.set(invViewMat);
-        this.uniforms.projMatVals.set(projMat);
-        this.uniforms.invProjMatVals.set(invProjMat);
-
-        this.uniforms.nearPlaneVal[0] = Camera.nearPlane;
-        this.uniforms.farPlaneVal[0] = Camera.farPlane;
-        this.uniforms.eyePosVals.set(this.cameraPos);
-
-        this.uniforms.screenWidthVal[0] = this.width;
-        this.uniforms.screenHeightVal[0] = this.height;
-    
+        const viewProjMat = mat4.mul(this.projMat, viewMat);
+        
+        // Update the view-projection matrix
+        this.uniforms.viewProjMat = viewProjMat;
+        
+        // Update the camera property vector
+        this.uniforms.camera = [
+            Camera.nearPlane,
+            Camera.farPlane,
+            0.0,
+            0.0,
+        ];
+        
+        // Update the inverse projection and view matrices
+        this.uniforms.inverse_projection = mat4.invert(this.projMat);
+        this.uniforms.inverse_view = mat4.invert(viewMat);
+        
         // DONE-1.1: upload `this.uniforms.buffer` (host side) to `this.uniformsBuffer` (device side)
         // check `lights.ts` for examples of using `device.queue.writeBuffer()`
         device.queue.writeBuffer(this.uniformsBuffer, 0, this.uniforms.camBuffer);
